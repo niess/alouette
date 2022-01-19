0110d6492698065c2fd51e0708a97c5342aec2f6
Valentin Niess <niess@in2p3.fr>
2022-01-19 12:36:06 +0100

    Export TAUOLA mass data to Python

--- alouette/tauola.py
+++ alouette/tauola.py
@@ -3,7 +3,7 @@ import numpy
 
 from ._core import ffi, lib
 
-__all__ = ('ipcht', 'wtmax',)
+__all__ = ('ipcht', 'parmas', 'wtmax',)
 
 
 class wtmax:
@@ -54,9 +54,6 @@ wtmax = wtmax()
 class ipcht:
     '''TAUOLA new currents flags'''
 
-    def __init__(self):
-        self.wtmax = wtmax
-
     @property
     def iver(self):
         '''Parametrisation version for decays to 2 and 3 pions.
@@ -79,3 +76,90 @@ class ipcht:
 
 # Override as class singleton
 ipcht = ipcht()
+
+
+class parmas:
+    '''TAUOLA mass parameters'''
+
+    @property
+    def amtau(self):
+        '''Tau mass, in GeV'''
+        return float(lib.tauola_parmas.amtau)
+
+    @property
+    def amnuta(self):
+        '''Tau neutrino mass, in GeV'''
+        return float(lib.tauola_parmas.amnuta)
+
+    @property
+    def amel(self):
+        '''Electron mass, in GeV'''
+        return float(lib.tauola_parmas.amel)
+
+    @property
+    def amnue(self):
+        '''Electron neutrino mass, in GeV'''
+        return float(lib.tauola_parmas.amnue)
+
+    @property
+    def ammu(self):
+        '''Muon mass, in GeV'''
+        return float(lib.tauola_parmas.ammu)
+
+    @property
+    def amnumu(self):
+        '''Muon neutrino mass, in GeV'''
+        return float(lib.tauola_parmas.amnumu)
+
+    @property
+    def ampiz(self):
+        '''Neutral pion mass, in GeV'''
+        return float(lib.tauola_parmas.ampiz)
+
+    @property
+    def ampi(self):
+        '''Charged pion mass, in GeV'''
+        return float(lib.tauola_parmas.ampi)
+
+    @property
+    def amro(self):
+        '''Rho mass, in GeV'''
+        return float(lib.tauola_parmas.amro)
+
+    @property
+    def gamro(self):
+        '''Rho decay width, in GeV'''
+        return float(lib.tauola_parmas.gamro)
+
+    @property
+    def ama1(self):
+        '''A1 mass, in GeV'''
+        return float(lib.tauola_parmas.ama1)
+
+    @property
+    def gama1(self):
+        '''A1 decay width, in GeV'''
+        return float(lib.tauola_parmas.gama1)
+
+    @property
+    def amk(self):
+        '''Charged kaon mass, in GeV'''
+        return float(lib.tauola_parmas.amk)
+
+    @property
+    def amkz(self):
+        '''Neutral kaon mass, in GeV'''
+        return float(lib.tauola_parmas.amkz)
+
+    @property
+    def amkst(self):
+        '''K*(892) mass, in GeV'''
+        return float(lib.tauola_parmas.amkst)
+
+    @property
+    def gamkst(self):
+        '''K*(892) decay width, in GeV'''
+        return float(lib.tauola_parmas.gamkst)
+
+# Override as class singleton
+parmas = parmas()
--- tests/test_tauola.py
+++ tests/test_tauola.py
@@ -2,7 +2,7 @@ import pytest
 
 import numpy
 import alouette
-from alouette.tauola import ipcht, wtmax
+from alouette.tauola import ipcht, parmas, wtmax
 
 
 def test_tauola():
@@ -41,3 +41,24 @@ def test_tauola():
     with pytest.raises(ValueError) as exc_info:
         ipcht.iver = 2
     assert 'bad iver' in exc_info.exconly()
+
+    # Check mass getters
+    assert parmas.amtau > 0
+    assert parmas.amnuta > 0
+    assert parmas.amel > 0
+    assert parmas.amnue == 0
+    assert parmas.ammu > 0
+    assert parmas.amnumu == 0
+    assert parmas.ampi > 0
+    assert parmas.ampiz > 0
+    assert parmas.amro > 0
+    assert parmas.gamro > 0
+    assert parmas.ama1 > 0
+    assert parmas.gama1 > 0
+    assert parmas.amk > 0
+    assert parmas.amkz > 0
+    assert parmas.amkst > 0
+    assert parmas.gamkst > 0
+
+    with pytest.raises(AttributeError):
+        parmas.amtau = 0
